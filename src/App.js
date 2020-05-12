3073d5a53ace183fbb6fdd9cef9c83f12eaf4660
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-05-12 16:06:02 +0200

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Home from "./components/Home";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
     <Home/>
     </div>
   );
 }
-
-export default App;
