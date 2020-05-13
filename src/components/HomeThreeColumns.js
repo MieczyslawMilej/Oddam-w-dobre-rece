c50d544b14924b3b5de5c74798311fc7de6e48e0
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-05-13 14:01:00 +0200

    Finished 3 columns section

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -2,10 +2,33 @@ import React from "react";
 
 export default function HomeThreeColumns() {
   return (
-    <div className="columns">
       <section className="columns">
-        <div className="wrapper">HomeThreeColumns</div>
+        <div className="wrapper">
+          <div className="three-box">
+            <p className="three-number">10</p>
+            <p className="three-title">oddanych worków</p>
+            <p  className="three-text">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+              enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+          </div>
+          <div className="three-box">
+            <p className="three-number">5 </p>
+            <p className="three-title">wspartych organizacji</p>
+            <p className="three-text">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+              enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+          </div>
+          <div className="three-box">
+            <p className="three-number">7</p>
+            <p className="three-title">zorganizowanych zbiórek</p>
+            <p className="three-text">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+              enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+          </div>
+        </div>
       </section>
-    </div>
   );
 }
