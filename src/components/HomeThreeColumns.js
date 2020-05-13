dde290c558acf355213b77d761f267eb4f348c27
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-05-13 16:20:04 +0200

    Created styles for 3 columns

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -6,24 +6,24 @@ export default function HomeThreeColumns() {
         <div className="wrapper">
           <div className="three-box">
             <p className="three-number">10</p>
-            <p className="three-title">oddanych worków</p>
-            <p  className="three-text">
+            <p className="title">oddanych worków</p>
+            <p  className="text">
               Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
               enim a elit viverra elementuma. Aliquam erat volutpat.
             </p>
           </div>
           <div className="three-box">
             <p className="three-number">5 </p>
-            <p className="three-title">wspartych organizacji</p>
-            <p className="three-text">
+            <p className="title">wspartych organizacji</p>
+            <p className="text">
               Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
               enim a elit viverra elementuma. Aliquam erat volutpat.
             </p>
           </div>
           <div className="three-box">
             <p className="three-number">7</p>
-            <p className="three-title">zorganizowanych zbiórek</p>
-            <p className="three-text">
+            <p className="title">zorganizowanych zbiórek</p>
+            <p className="text">
               Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
               enim a elit viverra elementuma. Aliquam erat volutpat.
             </p>
